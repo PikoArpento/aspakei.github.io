73ad6ce85b5e07bd5feb29924d26ce825869661a
PikoArpento <simo.torri104@gmail.com>
2025-03-23 12:33:56 +0100

    Delete mappa/città directory

--- mappa/città/meteo/neve.js
+++
@@ -1,8 +0,0 @@
-// 🔴 Rimuove gli elementi meteo precedenti
-document.querySelectorAll(".more-snow").forEach(el => el.remove());
-
-// 🔵 Crea il nuovo effetto neve
-const snowContainer = document.createElement("div");
-snowContainer.classList.add("more-snow", "weather-element");
-
-document.body.appendChild(snowContainer);
--- mappa/città/meteo/nuvole.js
+++
@@ -1,9 +0,0 @@
-// 🔴 Rimuove gli elementi meteo precedenti
-document.querySelectorAll(".clouds-1, .clouds-2, .clouds-3").forEach(el => el.remove());
-
-// 🔵 Crea le nuove nuvole
-for (let i = 1; i <= 3; i++) {
-    const cloud = document.createElement("div");
-    cloud.classList.add(`clouds-${i}`, "weather-element");
-    document.body.appendChild(cloud);
-}
